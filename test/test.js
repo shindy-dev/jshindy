b9eaed4ef6810cd90ba4bbf2cd8ef8b677ab4fd7
shindy-dev <shindy.developer@gmail.com>
2021-04-19 01:15:12 +0900

    スクリプト整理

--- test/test.js
+++ test/test.js
@@ -1,11 +1,35 @@
+// ！！！ 以下、変更しないでください。 ！！！
+var test = function () {
+    "use strict";
+
+    const scripts = document.getElementsByTagName('script');
+    const lastScript = scripts[scripts.length - 1];
+    const msg = `テスト関数をtest変数に代入してください。\n(${lastScript.src})`;
+    console.log(msg);
+    alert(msg);
+};
+window.executeTest = function () {
+    "use strict";
+
+    try {
+        test();
+    } catch (e) {
+        console.log(e.stack);
+        alert(e.stack);
+    };
+};
+// ！！！ 以上、変更しないでください。 ！！！
 
 
-import { calc } from "../mylib/sample.js";
+// 以下、テストサンプル
+import { calc } from "./sample.js";
 
 function testA() {
     alert(calc(30, 5));
 };
 
-
 // テストしたい関数名を代入してください。
-window.test = testA;
+test = testA;
+// 以上、テストサンプル
+
+
